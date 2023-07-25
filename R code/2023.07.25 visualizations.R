bdbc6dd15bf1adbba57fe9ecd184c86079bfe7cd
Hilary Rose Dawson <hrosedawson@gmail.com>
2023-07-25 10:20:33 +0200

    Added visualization of DURIN data

--- R code/2023.07.25 visualizations.R
+++ R code/2023.07.25 visualizations.R
@@ -10,4 +10,19 @@ durin.viz = durin.noerrors |>
   # Standardize traits
   mutate(trait = replace(trait,
                          trait == "leaf_thickness_1_mm" | trait == "leaf_thickness_2_mm" | trait == "leaf_thickness_3_mm",
-                         "leaf_thickness"))
+                         "leaf_thickness")) |>
+  # Factor the sites
+  mutate(siteID = factor(siteID, levels = c("Lygra", "Sogndal", "Senja", "Kautokeino")))
+
+# visualize data ----
+library(ggh4x)
+
+ggplot(durin.viz %>% filter(leaf_age != "" & habitat != ""),
+       aes(interaction(habitat, siteID), y = value, fill = leaf_age)) +
+  geom_boxplot() +
+  scale_fill_manual(values = c("grey40", "grey80")) +
+  facet_wrap(~ trait, scales = "free") +
+  scale_y_log10() +
+  scale_x_discrete(guide = "axis_nested") +
+  labs(x = "") +
+  theme_bw()
